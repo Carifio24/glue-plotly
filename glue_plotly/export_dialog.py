8fb78a0967385f5959241196dc55c7d454545b1b
Carifio24 <carifio24@gmail.com>
2022-11-07 10:30:14 -0500

    More experimentation.

--- glue_plotly/export_dialog.py
+++ glue_plotly/export_dialog.py
@@ -1,7 +1,7 @@
 
 import os
 
-from qtpy.QtWidgets import QDialog
+from qtpy.QtWidgets import QDialog, QLabel, QVBoxLayout
 from qtpy.QtCore import QTimer, Qt
 
 from glue.utils.qt import load_ui
@@ -12,23 +12,31 @@ class ExportDialog(QDialog):
     max_dots = 3
 
     def __init__(self, parent=None):
-        super(ExportDialog, self).__init__(parent=parent)
+        super(ExportDialog, self).__init__(parent=parent, flags=Qt.FramelessWindowHint)
 
-        self.ui = load_ui('export_dialog.ui', self,
-                          directory=os.path.dirname(__file__))
-        self.setWindowFlags(Qt.FramelessWindowHint)
+        # self.ui = load_ui('export_dialog.ui', self,
+        #                   directory=os.path.dirname(__file__))
+        # self.label = self.ui.label
 
-        self.label = self.ui.label
+        self.label = QLabel()
+        self.label.setText("Exporting to Plotly")
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+        self.setModal(True)
+        #self.setWindowFlags(Qt.FramelessWindowHint)
+
         ending_spaces = " " * self.max_dots
         if not self.label.text().endswith(ending_spaces):
             self.label.setText(self.label.text() + ending_spaces)
-        self.n_dots = 1
+        self.n_dots = 0
         self.timer = QTimer()
         self.timer.timeout.connect(self._on_timer_update)
         self.timer.start(500)
 
     def _on_timer_update(self):
-        self.n_dots = self.n_dots % self.max_dots or self.max_dots
+        self.n_dots = ((self.n_dots + 1) % (self.max_dots + 1)) or self.max_dots
+        print(self.n_dots)
         text = self.label.text()
         text = text[:-self.max_dots] + "." * self.n_dots + " " * (self.max_dots - self.n_dots)
         self.label.setText(text)
